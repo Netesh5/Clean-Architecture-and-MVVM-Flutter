77c896826478424f1cc96013743896455f3d56ec
Netesh5 <netesh.paudel@gmail.com>
2022-12-25 18:11:00 +0545

    on Boarding assets

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -2,4 +2,8 @@ const String imagePath = "assets/images";
 
 class ImageAssets {
   static const String splashLogo = "$imagePath/splash_logo.png";
+  static const String onBoardingImage1 = "$imagePath/onboarding_logo1.svg";
+  static const String onBoardingImage2 = "$imagePath/onboarding_logo2.svg";
+  static const String onBoardingImage3 = "$imagePath/onboarding_logo3.svg";
+  static const String onBoardingImage4 = "$imagePath/onboarding_logo4.svg";
 }
--- lib/presentation/resources/string_manager.dart
+++ lib/presentation/resources/string_manager.dart
@@ -1,3 +1,12 @@
 class AppString {
   static const String noRouteFound = "No Routes Found";
+  static const String onBoardingTitle1 = "On Boarding";
+  static const String onBoardingTitle2 = "On Boarding";
+  static const String onBoardingTitle3 = "On Boarding";
+  static const String onBoardingTitle4 = "On Boarding";
+
+  static const String onBoardingSubtitle1 = "On Boarding Subtitle";
+  static const String onBoardingSubtitle2 = "On Boarding Subtitle";
+  static const String onBoardingSubtitle3 = "On Boarding Subtitle";
+  static const String onBoardingSubtitle4 = "On Boarding Subtitle";
 }
