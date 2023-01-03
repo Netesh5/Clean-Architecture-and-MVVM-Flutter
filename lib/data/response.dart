61a17136a9db3ee8ff4b95d53d5fde275e90a0b0
Netesh5 <netesh.paudel@gmail.com>
2023-01-03 23:19:55 +0545

    json serialzation

---
+++ lib/data/response.dart
@@ -0,0 +1,58 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'response.g.dart';
+
+@JsonSerializable()
+class BaseResponse {
+  @JsonKey(name: "status")
+  int? status;
+  @JsonKey(name: "message")
+  String? message;
+}
+
+@JsonSerializable()
+class CustomerResponse {
+  @JsonKey(name: "id")
+  int? id;
+  @JsonKey(name: "name")
+  String? name;
+  @JsonKey(name: "notification")
+  String? notification;
+  CustomerResponse(this.id, this.name, this.notification);
+  factory CustomerResponse.fromJson(Map<String, dynamic> json) {
+    return _$CustomerResponseFromJson(json);
+  }
+  Map<String, dynamic> toJson() {
+    return _$CustomerResponseToJson(this);
+  }
+}
+
+@JsonSerializable()
+class ContactResponse {
+  @JsonKey(name: "phone")
+  int? phone;
+  @JsonKey(name: "email")
+  String? email;
+  ContactResponse(this.phone, this.email);
+  factory ContactResponse.fromJson(Map<String, dynamic> json) {
+    return _$ContactResponseFromJson(json);
+  }
+  Map<String, dynamic> toJson() {
+    return _$ContactResponseToJson(this);
+  }
+}
+
+@JsonSerializable()
+class AuthResponse extends BaseResponse {
+  @JsonKey(name: "customer")
+  CustomerResponse? customerResponse;
+  @JsonKey(name: "contacts")
+  ContactResponse? contactResponse;
+  AuthResponse(this.customerResponse, this.contactResponse);
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return _$AuthResponseFromJson(json);
+  }
+  Map<String, dynamic> toJson() {
+    return _$AuthResponseToJson(this);
+  }
+}
